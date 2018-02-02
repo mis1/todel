f3012c85a43dbceb275f4b2f74e668ff0df0598f
apple <imran.imran0@gmail.com>
2018-02-02 06:25:11 +0500

    we

--- metaShapeDefs.js
+++ metaShapeDefs.js
@@ -10,7 +10,7 @@ import {deepFreeze} from '../common/utility';
 /**
  * Defines symbols.
  * Each symbol is mapped to a color value. 
- * View layer defines colors and decides which symbol to map to which color. @see {@link module:viewConfig}
+ * View layer defines colors and decides which symbol to map to which color. @see module:viewConfig
  * @type {Array}
  */
 const symbols = []; //a-z
