1055bc7d88bcc67343f4ba945c9024e90aa9a6d3
apple <imran.imran0@gmail.com>
2018-02-02 06:19:23 +0500

    added jsdoc

---
+++ metaShapeDefs.js
@@ -0,0 +1,166 @@
+/** 
+ * @module metaShapeDefs
+ *
+ * Exports meta shapes. Metashap definition is used to render, rotate and color shapes.
+ * 
+ */
+
+import {deepFreeze} from '../common/utility';
+
+/**
+ * Defines symbols.
+ * Each symbol is mapped to a color value. 
+ * View layer defines colors and decides which symbol to map to which color. @see {@link module:viewConfig.colors}
+ * @type {Array}
+ */
+const symbols = []; //a-z
+for(let i=97; i<=122; i++){ 
+	symbols.push(String.fromCharCode(i));
+}
+
+/**
+ * Defines meta shapes. 
+ * Each meta shape has:
+ *  symbols @see 
+ *  , shapes array. 
+ * Shapes array defines 
+ */
+const metaShapeDefs={
+	ms1x2:{
+		symbols:[symbols[0],symbols[1]],
+		shapes:[
+			{
+				cells:[
+					[0,0], [0,1]
+				],
+				left:[0],
+				right:[1],
+				bottom:[0,1],
+
+			},
+			{
+				cells:[
+					[0,0], [-1,0]
+				],
+				left:[0,1],
+				right:[0,1],
+				bottom:[0],
+				symbols:[symbols[2]]
+			},
+		]
+	},
+	ms1x3:{
+		symbols:[],
+		shapes:[
+			{
+				cells:[
+					[0,0], [0,1], [0,-1]
+				],
+				left:[2],
+				right:[1],
+				bottom:[0,1,2],
+				symbols:[symbols[7],symbols[6],symbols[6]],
+			},
+			{
+				cells:[
+					[0,0], [-1,0], [-2, 0]
+				],
+				left:[0,1,2],
+				right:[0,1,2],
+				bottom:[0],
+				symbols:[symbols[8],symbols[9],symbols[8]],
+			},
+		]
+	},
+	ms2x2:{
+		symbols:[symbols[9]],
+		shapes:[
+			{
+				cells:[
+					[0,0], [0,1], [-1,0], [-1, 1]
+				],
+				left:[0, 2],
+				right:[1, 3],
+				bottom:[0,1],
+			},
+		]
+	},
+	msL2x2:{
+		symbols:[symbols[17],symbols[18],symbols[19]],
+		shapes:[
+			{
+				cells:[
+					[0,0], [0,1], [-1, 0]
+				],
+				left:[0,2],
+				right:[1,2],
+				bottom:[0,1],
+
+			},
+			{
+				cells:[
+					[0,1], [-1,1], [0,0]
+				],
+				left:[1,2],
+				right:[0,1],
+				bottom:[0,2],
+			},
+			{
+				cells:[
+					[-1,1],[-1,0],[0,1]
+				],
+				left:[1,2],
+				right:[0,2],
+				bottom:[1,2],
+			},
+			{
+				cells:[
+					[-1,0],[0,0], [-1,1]
+				],
+				left:[0,1],
+				right:[1,2],
+				bottom:[1,2],
+			},
+		]
+	},
+	msT3x3:{
+		symbols:[symbols[19]],
+		shapes:[
+			{
+				cells:[
+					[0,-1], [0,0], [0,1],[-1,0]
+				],
+				left:[0,3],
+				right:[2,3],
+				bottom:[0,1,2]
+			},
+			{
+				cells:[
+					[0,0], [-1,0], [-2, 0], [-1,-1]
+				],
+				left:[0,2, 3],
+				right:[0,1,2],
+				bottom:[0,3],
+			},
+			{
+				cells:[
+					[-1,1],[-1,0],[-1,-1], [0,0]
+				],
+				left:[2,3],
+				right:[0,3],
+				bottom:[0,2,3],
+			},
+			{
+				cells:[
+					[-2,0],[-1,0], [0,0],[-1,1]
+				],
+				left:[0,1,2],
+				right:[0,2,3],
+				bottom:[2,3],
+			},
+		]
+	},
+};
+
+deepFreeze(metaShapeDefs);
+export default metaShapeDefs;
---
+++ viewConfig.js
@@ -0,0 +1,26 @@
+/**
+ * @module viewConfig
+ *
+ * Defines and exports configuration used by the views.
+ * 
+ */
+
+/**
+ * Defines colors.
+ * Each symbol is mapped to a color. @see {@link module:metaShapeDefs~symbols}
+ * @type {Array}
+ */
+const colors = ['#5F9EA0', '#FF69B4', '#0f0', '#0ff', '#f0f', '#7FFF00', '#20B2AA', '#6B8E23', '#DA70D6', '#DB7093', '#DDA0DD', '#800080'];
+
+const clearColor = '#f3f3f3';
+
+const cellSize = 33;
+
+const maxBoardBorderWidth = 10;
+
+const maxBoardWidth = 480;
+
+const maxBoardHeight = 640;
+
+export {colors, clearColor, cellSize, maxBoardBorderWidth, maxBoardWidth, maxBoardHeight};
+
